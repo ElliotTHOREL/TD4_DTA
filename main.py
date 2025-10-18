6b17bd23e913cec9259cf53451c72159c23cd3cf
dscholten <d.scholten@student.science.ru.nl>
2025-10-18 13:08:55 +0200

    sdsd

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ def retrieve_data(filename):
 if __name__ == "__main__":
     print("retrieving data", time.time())
     start = time.time()
-    simplex_set = retrieve_data("filtration_A.txt")
+    simplex_set = retrieve_data("filtration_B.txt")
     bars = simplex_set.compute_bars()
     print("done, time:", time.time() - start)
