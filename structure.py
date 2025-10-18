8fc415501753d2c861f4aa6471c01c0468d54b67
Elliot THOREL <elliot.thorel@polytechnique.edu>
2025-10-18 12:18:14 +0200

    matrix echelon forms

--- structure.py
+++ structure.py
@@ -9,6 +9,9 @@ class simplex_set:
     def get_matrix(self):
         pass
 
+
+
+
 class simplex:
     def __init__(self, apparition_time, dimension, under_vertices):
         self.apparition_time = apparition_time
@@ -28,8 +31,20 @@ class simplex:
 
 
 class matrix:
-    def __init__(self):
-        self.columns = []   # list of ints (dec repr of bitstring)
+    """
+        class to represent a square matrix in Z/2Z
+
+        columns is a list of ints 
+            - each int codes a column of the matrix (bits encoding)
+        size is the size of the matrix
+
+    """
+    def __init__(self, columns=[]):
+        self.columns = columns   # list of ints (dec repr of bitstring)
+        self.size = len(columns)
+
+        self.zero_columns = None #Not computed yet (make sense only after the matrix is in echelon form)
+        self.low1 = None #Not computed yet (make sense only after the matrix is in echelon form)
 
     def __index__(self, coord):
         # coord = (i, j)
@@ -37,7 +52,8 @@ class matrix:
         if not isinstance(coord, tuple):
             raise Exception("wrong type")
         
-        if coord[1] < 0 or coord[1] >= len(self.columns) or coord[0] < 0 or coord[0] > len(self.columns):
+        if coord[1] < 0 or coord[1] >= self.size or coord[0] < 0 or coord[0] > self.size:
+            print(coord)
             raise Exception("Index out of range")
         
         return int(self.columns[coord[1]] & 2**coord[0] > 0)
@@ -51,17 +67,46 @@ class matrix:
         return return_string
 
     def make_echelon_form(self):
-        pass
+        """Method to transform the matrix into echelon form"""
+        self.zero_columns = set()   # set of column indices that are zero-columns 
+                                    # i.e. actual cycles
+        self.low1 ={}   # map a row to the column that has its lowest 1 in it
+                        # i.e. map a cycle to its death 
 
-    def is_echelon_form(self):
-        pass
+
+        for j in range(self.size):
+            column = self.columns[j]
+            while True:
+                lower1 =column.bit_length()-1 # compute the row index of the lowest 1
+
+                if lower1 ==-1: #zero-column
+                    self.zero_columns.add(j)
+                    self.columns[j] = 0
+                    break
+                elif lower1 in self.low1:
+                    # We compute the sum with the given column (i.e. xor operation)
+                    column = column ^ self.columns[self.low1[lower1]]
+                else: 
+                    self.low1[lower1] = j
+                    self.columns[j] = column
+                    break
 
     def bars(self):
-        pass 
+        """Method to compute the bars of the matrix"""
+        bars = []
+        for cycle in self.zero_columns:
+            if cycle in self.low1:
+                bars.append((cycle, self.low1[cycle]))
+            else:
+                bars.append((cycle, float('inf')))
 
+        return bars
 
-matr = matrix()
-matr.columns = [int(b'0110', 2), int(b'1011', 2), int(b'1100', 2), int(b'0101', 2)]
-#matr.columns = [1+4, 3, 4]
-print(matr)
-print(int(b'101', 2))
+if __name__ == "__main__":
+    columns = [int(b'0110', 2), int(b'1011', 2), int(b'1100', 2), int(b'0101', 2)]
+    matr = matrix(columns)
+    #matr.columns = [1+4, 3, 4]
+    print(matr)
+    matr.make_echelon_form()
+    print(matr)
+    print(matr.bars())
