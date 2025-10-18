7544a8edb1422171bb53c9a7c389fabf1e3cbe7d
dscholten <d.scholten@student.science.ru.nl>
2025-10-18 10:46:23 +0200

    repr

--- structure.py
+++ structure.py
@@ -42,7 +42,14 @@ class matrix:
         
         return int(self.columns[coord[1]] & 2**coord[0] > 0)
 
-    
+    def __repr__(self):
+        return_string = ""
+
+        for i in range(len(self.columns)):
+            return_string = ''.join(([return_string] + [str(self.__index__((i, j))) for j in range(len(self.columns))] + ['\n']))
+
+        return return_string
+
     def make_echelon_form(self):
         pass
 
@@ -51,3 +58,10 @@ class matrix:
 
     def bars(self):
         pass 
+
+
+matr = matrix()
+matr.columns = [int(b'0110', 2), int(b'1011', 2), int(b'1100', 2), int(b'0101', 2)]
+#matr.columns = [1+4, 3, 4]
+print(matr)
+print(int(b'101', 2))
