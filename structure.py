0d982be18a08541486feed5c730c58bf6177f473
dscholten <d.scholten@student.science.ru.nl>
2025-10-18 10:08:41 +0200

    sdsdfsdf

--- structure.py
+++ structure.py
@@ -1,6 +1,13 @@
 class simplex_set:
-    def __init__(self, vertices):
-        self.vertices = sorted(vertices) 
+    def __init__(self, simplexes):
+        self.simplexes = sorted(simplexes) 
+        self.matrix = self.get_matrix()
+
+    def compute_bars(self):
+        pass
+
+    def get_matrix(self):
+        pass
 
 class simplex:
     def __init__(self, apparition_time, dimension, under_vertices):
@@ -20,3 +27,26 @@ class simplex:
         return self.apparition_time == other.apparition_time and self.dimension == other.dimension and self.under_vertices == other.under_vertices
 
 
+class matrix:
+    def __init__(self):
+        self.columns = []   # list of ints (dec repr of bitstring)
+
+    def __index__(self, coord):
+        # coord = (i, j)
+
+        if not isinstance(coord, tuple):
+            raise Exception("wrong type")
+        
+        if coord[1] < 0 or coord[1] >= len(self.columns) or coord[0] < 0 or coord[0] > len(self.columns):
+            raise Exception("Index out of range")
+        
+        return int(self.columns[coord[1]] & 2**coord[0] > 0)
+    
+    def make_echelon_form(self):
+        pass
+
+    def is_echelon_form(self):
+        pass
+
+    def bars(self):
+        pass 
