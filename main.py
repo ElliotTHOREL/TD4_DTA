cea62814dc975b51213a5e97bd4f3a2301cc0ba0
dscholten <d.scholten@student.science.ru.nl>
2025-10-22 09:44:20 +0200

    Clean up code

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from structure import Simplex, Simplex_set
 import time
 
-def retrieve_simplex(line):
-    info = list(map(lambda x : x.replace('\n', ''), line.split(" ")))
-    apparition_time = float( info[0] )
-    dimension = int( info[1] )
-    under_vertices = sorted(info[2:])
-    return Simplex(apparition_time, dimension, under_vertices)
+def retrieve_simplex(line: str):
+    apparition_time, dimension, *under_vertices = list(map(lambda x : x.replace('\n', ''), line.split(" ")))
+
+    return Simplex(float(apparition_time), int(dimension), sorted(under_vertices))
 
 
-def retrieve_data(filename):
+def retrieve_data(filename: str):
     simplexes = []
+
     with open(filename, 'r') as file:
         for line in file.readlines():
             simplexes.append( retrieve_simplex(line) )
+
     return Simplex_set(simplexes)
 
-if __name__ == "__main__":
-    print("retrieving data", time.time())
-    start = time.time()
-    simplex_set = retrieve_data("test1.txt")
-    bars = simplex_set.compute_bars()
-    print("done, time:", time.time() - start)
 
+if __name__ == "__main__":
+    c = 'B'
+
+    file_name = f"filtration_{c}"
+    start = time.perf_counter()
+
+    simplex_set = retrieve_data(f"{file_name}.txt")
+    bars = simplex_set.compute_bars()
+
+    print(f"Finished, time: {time.perf_counter() - start:.6f} seconds")
+
+    with open(f"{file_name}.out", 'w') as f:
+        for bar in bars:
+            f.write(' '.join(list(map(str, bar)) + ['\n']))
--- structure.py
+++ structure.py
@@ -1,58 +1,7 @@
-from stringprep import in_table_a1
-import time
-
-class Simplex_set:
-    def __init__(self, simplexes):
-        print("sorting the simplexes", time.time())
-        self.simplexes = sorted(simplexes) 
-        print("making the matrix", time.time())
-        self.matrix = self.get_matrix()
-
-    def compute_bars(self):
-        print("making ecehelon form", time.time())
-        self.matrix.make_echelon_form()
-        print("getting bars", time.time())
-        bars = self.matrix.bars()  # list of (indexA, indexB) tuples --> (dim sigma_indexA, f(indexA), f(indexB))
-
-        print("converting bars", time.time())
-        converted_bars = []
-
-        for indexA, indexB in bars:
-            dimA = self.simplexes[indexA].dimension
-            fA = self.simplexes[indexA].apparition_time
-            fB = self.simplexes[indexB].apparition_time if indexB != float('inf') else float('inf')
-
-            converted_bars.append((dimA, fA, fB))
-
-        return converted_bars
-
-
-    def get_matrix(self):
-        vertex_to_index = {}
-        print("index dictionary", time.time())
-        for index, simplex in enumerate(self.simplexes):
-            vertex_to_index[tuple(simplex.under_vertices)] = index
-
-        print("filling columns", time.time())
-        columns = []
-        for index_simp, simplex in enumerate(self.simplexes):
-            column = []
-
-            for index_vert, vertex in enumerate(simplex.under_vertices):
-                face = tuple(simplex.under_vertices[:index_vert] + simplex.under_vertices[index_vert+1:])
-
-                if face in vertex_to_index.keys():
-                    column .append( vertex_to_index[face] )
-            
-
-            columns.append(sorted(column))
-
-        return Matrix(columns)
-
-
+from typing import Any
 
 class Simplex:
-    def __init__(self, apparition_time, dimension, under_vertices):
+    def __init__(self, apparition_time: float, dimension: int, under_vertices: list[Any]):
         self.apparition_time = apparition_time
         self.dimension = dimension
         self.under_vertices = under_vertices
@@ -72,25 +21,127 @@ class Simplex:
         return f"f(sigma) = {self.apparition_time}, dim = {self.dimension}, sigma = ({self.under_vertices})"
 
 
+class Simplex_set:
+    def __init__(self, simplexes: list[Simplex]):
+        self.simplexes = sorted(simplexes) 
+        self.matrix = self.get_matrix()
+
+    def compute_bars(self):
+        self.matrix.make_echelon_form()
+        bars = self.matrix.bars()           # list of (indexA, indexB) tuples
+
+        converted_bars = []
+
+        for indexA, indexB in bars:
+            dimA = self.simplexes[indexA].dimension
+            fA = self.simplexes[indexA].apparition_time
+            fB = self.simplexes[indexB].apparition_time if indexB != float('inf') else float('inf')
+
+            converted_bars.append((dimA, fA, fB))
+
+        return converted_bars
+
+
+    def get_matrix(self):
+        vertex_to_index = {}
+        
+        for index, simplex in enumerate(self.simplexes):
+            vertex_to_index[tuple(simplex.under_vertices)] = index
+
+        columns = []
+        for simplex in (self.simplexes):
+            col_values = set()
+            col_max = -1
+
+            for index_vert in range(len(simplex.under_vertices)):
+                face = tuple(simplex.under_vertices[:index_vert] + simplex.under_vertices[index_vert+1:])
+
+                if face in vertex_to_index:
+                    col_values.add( vertex_to_index[face] )
+                    col_max = max(col_max, vertex_to_index[face])
+            
+            columns.append(Column(values=col_values, pmax=col_max))
+
+        return Matrix(columns)
+
+class Column:
+    def __init__(self, values: set[int] = set(), pmax: int = -1):
+        self.__values = values
+        self.__max = pmax if pmax != -1 else (max(values) if values else -1)
+
+    def __eq__(self, other):
+        return self.__values == other.get_values()
+    
+    def __repr__(self):
+        return f"{str(self.__values)}, max = {self.__max}"
+    
+    def __xor__(self, other):
+        if not isinstance(other, Column):
+            raise Exception (f"{other.__name__()} must be of type Column but is of type {type(other)}")
+        
+        values = self.__values ^ other.get_values()
+
+        pmax = None  # we start with an unknown max state
+        other_max = other.get_max(allow_unknown=True)
+        if self.__max is not None and other_max is not None:  # both values have a known max
+            if self.__max != other_max:  # if the maxes are distinct we can infer a new max value.
+                pmax = max(self.__max, other_max)
+
+        return Column(values=values, pmax=pmax)
+    
+    def __ixor__(self, other):
+        self.__values ^= other.get_values()
+
+        self.__max = None
+        # we put max on None to signify it is unknown. We could check for an efficient method
+        # if the maxes of the columns do not collide, but that is never the case in our algorithm
+        return self
+
+    
+    def __len__(self):
+        return len(self.__values)
+    
+    def __contains__(self, a):
+        return a in self.__values
+
+    def set_values(self, values: set[int], pmax: int = -1):
+        self.__values = values
+        self.__max = pmax if pmax != -1 else (max(values) if values else -1)
+
+    def get_values(self):
+        return self.__values
+
+    def get_max(self, allow_unknown: bool = False):
+        if not allow_unknown and self.__max is None:
+            # if we have an unknown max value we must compute it here
+            self.__max = max(self.__values) if self.__values else -1
+        
+        return self.__max
+    
+
 class Matrix:
-    def __init__(self, columns = []):
-        self.columns = columns  # list of ints (dec repr of bitstring)
-        self.size = len(columns)
-
+    def __init__(self, columns: list[Column] = []):
+        self.columns = None
+        self.size = None
         self.zero_columns = None
-        self.low1 = None
+        self.pivots = None
 
-    def __index__(self, coord):
-        # coord = (i, j)
+        if columns:
+            self.set_columns(columns=columns)
 
+    def set_columns(self, columns: list[Column]):
+        self.columns = columns
+        self.size = len(columns) if columns else None
+
+    def __index__(self, coord: tuple[int, int]):     # coord: (i, j)
         if not isinstance(coord, tuple):
             raise Exception("wrong type")
         
         if coord[1] < 0 or coord[1] >= self.size or coord[0] < 0 or coord[0] > self.size:
-            print(coord)
+            # we assume a square matrix
             raise Exception("Index out of range")
         
-        return int(coord[0] in columns[coord[1]])
+        return int(coord[0] in self.columns[coord[1]])
 
     def __repr__(self):
         return_string = ""
@@ -100,77 +151,53 @@ class Matrix:
 
         return return_string
 
-    def add_column(self, c1, c2):
-        """
-        Method to add the column c2 to the column c1
-        """
-        new_column=[]
-        i1=0
-        i2=0
-
-        while i1<len(c1) and i2<len(c2):
-            if c1[i1] == c2[i2]:
-                i1+=1
-                i2+=1
-            elif c1[i1] < c2[i2]:
-                new_column.append(c1[i1])
-                i1+=1
-            else:
-                new_column.append(c2[i2])
-                i2+=1
-
-        return new_column + c1[i1:] + c2[i2:]
-
-
-
-    def get_lower1(self, c):
-        """
-        Method to get the lower1 of the jth column 
-        """
-        if c==[]:
-            return -1
-        return c[-1]
+    def add_column(self, c1: Column, c2: Column):
+        # returns the XOR of two columns, adding them element-wise mod 2
+        return c1 ^ c2
+    
+    def get_lower1(self, c: Column):
+        # returns the row index of the 1 with the lowest position in the matrix, given a column c.
+        # If the column is empty return -1
+        return c.get_max()
 
     def make_echelon_form(self):
-        """Method to transform the matrix into echelon form"""
-        self.zero_columns = set()   # set of column indices that are zero-columns 
-                                    # i.e. actual cycles
-        self.low1 ={}   # map a row to the column that has its lowest 1 in it
-                        # i.e. map a cycle to its death 
+        """
+        Transform the matrix into echelon form using Gaussian elimination.
+        """
+
+        self.zero_columns = set()  # set of indexes of zero-columns
+        self.pivots = {}           # dict[index : column with pivot on index]
+
         for j in range(self.size):
             column = self.columns[j]
-            while True:
-                lower1 = self.get_lower1(column) # compute the row index of the lowest 1
+            lower1 = self.get_lower1(column)
 
-                if lower1 ==-1: #zero-column
+            while True:
+                # current column is empty
+                if lower1 == -1:
                     self.zero_columns.add(j)
                     self.columns[j] = column
                     break
-                elif lower1 in self.low1:
-                    # We compute the sum with the given column (i.e. xor operation)
-                    column = self.add_column(column, self.columns[self.low1[lower1]])
-                else: 
-                    self.low1[lower1] = j
+
+                # there exists a column to the left with a "pivot collision"
+                elif lower1 in self.pivots:
+                    column ^= self.columns[self.pivots[lower1]]
+                    lower1 = self.get_lower1(column)                # update the stored lowest 1 for this column
+                
+                # current column has a pivot element with no collisions to the left
+                else:
+                    self.pivots[lower1] = j     # register current column as holder of pivot on index lower1 
                     self.columns[j] = column
                     break
 
+
     def bars(self):
-        """Method to compute the bars of the matrix"""
+        """Method to compute the bars of the matrix, independent of the simplex to index bijection"""
         bars = []
         for cycle in self.zero_columns:
-            if cycle in self.low1:
-                bars.append((cycle, self.low1[cycle]))
+            if cycle in self.pivots:
+                bars.append((cycle, self.pivots[cycle]))
             else:
                 bars.append((cycle, float('inf')))
 
         return bars
-
-if __name__ == "__main__":
-    columns = [int(b'0110', 2), int(b'1011', 2), int(b'1100', 2), int(b'0101', 2)]
-    matr = Matrix(columns)
-    #matr.columns = [1+4, 3, 4]
-    print(matr)
-    matr.make_echelon_form()
-    print("jdhdhf")
-    print(matr)
-    print(matr.bars())
